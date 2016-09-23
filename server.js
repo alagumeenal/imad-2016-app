2400958b5d55f9537f47fb53d5820ff6116a10ce
alagumeenal <ameena167@gmail.com>
2016-09-23 19:32:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.send('Article two requested will be a server two ');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.send('Article three requested will be a server three');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
