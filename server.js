fd933f05521e788820d4735be775a9d301967d8b
alagumeenal <ameena167@gmail.com>
2016-09-24 09:55:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var content={
+var articleone={
     title: 'Article one I ALAGUMEENAL'
     heading:'article-one'
     content: <p>
