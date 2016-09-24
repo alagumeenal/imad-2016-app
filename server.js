d977a1cffeb949c06ada23b68e75100b94f2d85a
alagumeenal <ameena167@gmail.com>
2016-09-24 10:08:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var articleone={
                 This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one. This content display article one.
             </p>'
 };
+function createFunction()
 var htmltemplate='
 
 <html>
